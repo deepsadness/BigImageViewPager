044e7747167a42ab920562c182782a061d4d2480
deepsadness <a295756092@gmail.com>
2019-10-11 11:59:32 +0800

    修改沉浸式配置

--- library/src/main/java/cc/shinichi/library/view/ImagePreviewActivity.java
+++ library/src/main/java/cc/shinichi/library/view/ImagePreviewActivity.java
@@ -101,7 +101,7 @@ public class ImagePreviewActivity extends AppCompatActivity implements Handler.C
                             | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                             | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                             | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+//                            | View.SYSTEM_UI_FLAG_FULLSCREEN
                             | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
             );
 
