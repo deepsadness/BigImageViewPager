912bb2e3a1d130ba13f0d6914efbabeca6abdd95
deepsadness <a295756092@gmail.com>
2019-10-11 11:46:06 +0800

    修改沉浸式配置

--- library/src/main/java/cc/shinichi/library/view/ImagePreviewActivity.java
+++ library/src/main/java/cc/shinichi/library/view/ImagePreviewActivity.java
@@ -101,14 +101,14 @@ public class ImagePreviewActivity extends AppCompatActivity implements Handler.C
                             | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                             | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                             | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-//                            | View.SYSTEM_UI_FLAG_FULLSCREEN
-//                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
             );
 
 //            window.getDecorView()
 //                .setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.setStatusBarColor(Color.TRANSPARENT);
+            window.setStatusBarColor(Color.BLACK);
             window.setNavigationBarColor(Color.TRANSPARENT);//将导航栏设置为透明色
         } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
